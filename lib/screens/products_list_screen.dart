1b87c2dd383a878a00ba30207ee2bba3a6eaa605
Jaganathan P <infantjagan@gmail.com>
2022-06-26 12:07:52 +0530

    add search functionality
    add search functionality

--- lib/screens/products_list_screen.dart
+++ lib/screens/products_list_screen.dart
@@ -16,7 +16,8 @@ class ProductListScreen extends StatefulWidget {
 
 class _ProductListScreenState extends State<ProductListScreen> {
   
-  List<Product> products = [];
+  List<Product> allProducts = [];
+  List<Product> filteredProducts = [];
 
   Future<void> readJsonFile() async {
 
@@ -26,10 +27,29 @@ class _ProductListScreenState extends State<ProductListScreen> {
     var list = productData["items"] as List<dynamic>;
 
     setState(() {
-      products = list.map((e) => Product.fromJson(e)).toList();
+      allProducts = [];
+      allProducts = list.map((e) => Product.fromJson(e)).toList();
+      filteredProducts = allProducts;
     });
 
   }
+
+  void _runFilter(String searchKeyword) {
+    List<Product> results = [];
+
+    if(searchKeyword.isEmpty) {
+      results = allProducts;
+    } else {
+      results = allProducts.where((element) => element.name.toLowerCase().contains(searchKeyword.toLowerCase())).toList();
+    }
+
+    // refresh the UI
+    setState(() {
+      filteredProducts = results;
+    });
+
+  }
+
 
   @override
   Widget build(BuildContext context) {
@@ -42,34 +62,90 @@ class _ProductListScreenState extends State<ProductListScreen> {
             child: ElevatedButton(onPressed: readJsonFile, child: Text("Load Products")),
           ),
 
-          if (products.length > 0) 
-            Expanded(
-              child: ListView.builder(
-                itemCount: products.length,
-                itemBuilder: (BuildContext context, index) {
-                  return Card(
-                    margin: EdgeInsets.all(15.0),
-                    color: Colors.greenAccent,
-                    child: ListTile(
-                      title: Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: Text(products[index].name),
-                      ),
-                      subtitle: Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: Text(products[index].price.toString(), style: TextStyle(fontWeight: FontWeight.bold),),
-                      ),
-                      onTap: () {
-                        // print(jsonEncode(products[index]));
-                        Navigator.of(context).pushNamed(ProductDetailScreen.routeName, arguments: jsonEncode(products[index]));
-                      },
-                    )
-                  );
-                },
+          // if (allProducts.length > 0) 
+
+            Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: Column(
+                children: [
+                  SizedBox(height: 10,),
+                  TextField(
+                    onChanged: (value) => _runFilter(value),
+                    decoration: InputDecoration(labelText: 'Search', suffixIcon: Icon(Icons.search)),
+                  ),
+                  SizedBox(height: 10,),
+                ],
               ),
-            )
-           else 
-              Container(child: Text("No products"),)
+            ),
+            Expanded(
+              child: SingleChildScrollView(
+                child: ListView.builder(
+                  shrinkWrap: true,
+                  itemCount: filteredProducts.length,
+                  itemBuilder: (BuildContext context, index) {
+                    return Dismissible(
+                      key: ValueKey(filteredProducts[index].id.toString()),
+                      background: Container(
+                        color: Colors.redAccent,
+                        child: Icon(Icons.delete, color: Colors.white, size: 40),
+                        padding: EdgeInsets.all(8.0),
+                        margin: EdgeInsets.all(8.0),
+                      ),
+                      direction: DismissDirection.endToStart,
+                      confirmDismiss: (direction) {
+              
+                        return showDialog(
+                                  context: context, 
+                                  builder: (ctx) => AlertDialog(
+                                    title: Text("Please Confirm"),
+                                    content: Text("Are you sure you want to delete?"),
+                                    actions: [
+                                      ElevatedButton(onPressed: (){
+                                        Navigator.of(ctx).pop(false);
+                                      }, child: Text("Cancel")),
+                                      ElevatedButton(onPressed: (){
+                                        Navigator.of(ctx).pop(true);
+                                      }, child: Text("Delete")),
+                                    ],
+                                  ),
+                              );
+              
+              
+                      },
+                      onDismissed: (DismissDirection direction) {
+              
+                        if(direction == DismissDirection.endToStart) {
+                          filteredProducts.removeAt(index);
+                        }
+              
+                      },
+                      child: Card(
+                        margin: EdgeInsets.all(15.0),
+                        color: Colors.greenAccent,
+                        child: ListTile(
+                          title: Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Text(filteredProducts[index].name),
+                          ),
+                          subtitle: Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Text(filteredProducts[index].price.toString(), style: TextStyle(fontWeight: FontWeight.bold),),
+                          ),
+                          onTap: () {
+                            // print(jsonEncode(products[index]));
+                            Navigator.of(context).pushNamed(ProductDetailScreen.routeName, arguments: jsonEncode(filteredProducts[index]));
+                          },
+                        )
+                      ),
+                    );
+                  },
+                ),
+              ),
+            ),
+            SizedBox(height: 30,)
+            
+          // else 
+            // Container(child: Text("No products"),)
 
         ],
       ),
